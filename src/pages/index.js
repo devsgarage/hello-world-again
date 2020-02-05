b8683fad57b4bf520e1b2635d012afb40887443e
Ryan Overton <ryan.overton@outlook.com>
2020-02-05 10:03:55 -0600

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => <div style={{ color: `red` }}>
+    <Header headerText="Home"></Header>
+    <h1>Hello hot reloaded!</h1>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+</div>
